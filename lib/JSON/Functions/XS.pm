5d5c9e928eaff89cdd5ba7b3425083064324e6c2
Wakaba <wakaba@suikawiki.org>
2014-04-21 15:51:38 +0900

    Drop Exporter::Lite dependency

--- lib/JSON/Functions/XS.pm
+++ lib/JSON/Functions/XS.pm
@@ -1,13 +1,13 @@
 package JSON::Functions::XS;
 use strict;
 use warnings;
-our $VERSION = '1.2';
-use Exporter::Lite;
+no warnings 'utf8';
+our $VERSION = '3.0';
+use Carp;
 use JSON::XS;
 use Encode;
 
-our @EXPORT = qw();
-our @EXPORT_OK = qw(
+our @EXPORT = qw(
     json_chars2perl
     json_bytes2perl
     file2perl
@@ -16,6 +16,17 @@ our @EXPORT_OK = qw(
     perl2json_bytes
     perl2json_bytes_for_record
 );
+
+sub import ($;@) {
+  my $from_class = shift;
+  my ($to_class, $file, $line) = caller;
+  no strict 'refs';
+  for (@_ ? @_ : @{$from_class . '::EXPORT'}) {
+    my $code = $from_class->can ($_)
+        or croak qq{"$_" is not exported by the $from_class module at $file line $line};
+    *{$to_class . '::' . $_} = $code;
+  }
+} # import
 
 my $jsonc = JSON::XS->new->allow_blessed->convert_blessed->allow_nonref;
 my $jsoncr = JSON::XS->new->allow_blessed->convert_blessed->allow_nonref->pretty->canonical;
@@ -105,3 +116,14 @@ sub perl2json_bytes_for_record ($) {
 }
 
 1;
+
+=head1 LICENSE
+
+Copyright 2009-2011 Hatena <http://www.hatena.ne.jp/>.
+
+Copyright 2012-2014 Wakaba <wakaba@suikawiki.org>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
